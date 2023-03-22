f995ad237afa9dd095e475e9df6f6f513f9a25ca
mizt <info@mizt.org>
2023-03-23 08:36:15 +0900

    Add jit.blur

---
+++ jit.blur/Blur.h
@@ -0,0 +1,423 @@
+class Blur {
+    
+    private:
+    
+        const unsigned char thread = 4;
+            
+        bool _init = false;
+        long _width = 0;
+        long _height = 0;
+    
+        long _radius = 1;
+    
+        dispatch_group_t _group = dispatch_group_create();
+        dispatch_queue_t _queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH,0);
+            
+        unsigned int *_xy = nullptr;
+        unsigned int *_yx = nullptr;
+    
+        unsigned int **_rgb = new unsigned int *[thread];
+        unsigned int **_buffer = new unsigned int *[thread];
+    
+        void reset() {
+            
+            if(this->_xy) delete[] this->_xy;
+            this->_xy = new unsigned int[this->_width*this->_height];
+
+            if(this->_yx) delete[] this->_yx;
+            this->_yx = new unsigned int[this->_width*this->_height];
+
+            for(int k=0; k<thread; k++) {
+                if(this->_rgb[k]) delete[] this->_rgb[k];
+                this->_rgb[k] = new unsigned int[(this->_width*this->_height)?this->_width*3:this->_height*3];
+            }
+                                        
+            for(int k=0; k<thread; k++) {
+                if(this->_buffer[k]) delete[] this->_buffer[k];
+                this->_buffer[k] = new unsigned int[(int)(ceil((this->_width*this->_height)/(double)thread))];
+            }
+        }
+    
+        void blurX(unsigned int *dst, unsigned int *src, unsigned int *buffer, long w, long h, long begin, long end) {
+            
+            long radius = this->_radius;
+            if(radius<=1) radius = 1;
+            else if(radius>=(w>>2)) radius = w>>2;
+            else if(radius>=(h>>2)) radius = h>>2;
+
+            double weight = 1.0/(double)(radius*2+1);
+            
+            unsigned int sr = 0;
+            unsigned int sg = 0;
+            unsigned int sb = 0;
+            
+            unsigned int *buf = buffer;
+
+            for(long i=begin; i<end; i++) {
+                
+                sr = sg = sb = 0;
+                
+                unsigned int *p = src+i*w;
+                
+                for(long k=-(radius+1); k<radius; k++) {
+                    long j2 = 0+k;
+                    if(j2<0) j2=0;
+                    else if(j2>=w-1) j2=w-1;
+                    unsigned int pixel = *(p+j2);
+#ifdef BGRA
+                    sb+=(pixel>>24)&0xFF;
+                    sg+=(pixel>>16)&0xFF;
+                    sr+=(pixel>>8)&0xFF;
+#else
+                    sb+=(pixel>>16)&0xFF;
+                    sg+=(pixel>>8)&0xFF;
+                    sr+=(pixel)&0xFF;
+#endif
+                    
+                }
+                        
+                for(long j=0; j<w; j++) {
+        // sub
+                    long j2 = j-(radius+1);
+                    if(j2<0) j2=0;
+                    unsigned int pixel = *(p+j2);
+#ifdef BGRA
+                    sb-=(pixel>>24)&0xFF;
+                    sg-=(pixel>>16)&0xFF;
+                    sr-=(pixel>>8)&0xFF;
+#else
+                    sb-=(pixel>>16)&0xFF;
+                    sg-=(pixel>>8)&0xFF;
+                    sr-=(pixel)&0xFF;
+#endif
+                    
+        // add
+                    j2 = j+radius;
+                    if(j2>=w-1) j2=w-1;
+                    pixel = *(p+j2);
+#ifdef BGRA
+                    sb+=(pixel>>24)&0xFF;
+                    sg+=(pixel>>16)&0xFF;
+                    sr+=(pixel>>8)&0xFF;
+#else
+                    sb+=(pixel>>16)&0xFF;
+                    sg+=(pixel>>8)&0xFF;
+                    sr+=(pixel)&0xFF;
+#endif
+
+                    unsigned char r = sr*weight;
+                    unsigned char g = sg*weight;
+                    unsigned char b = sb*weight;
+
+#ifdef BGRA
+                    *buf++ = b<<24|g<<16|r<<8|0xFF;
+#else
+                    *buf++ = 0xFF000000|b<<16|g<<8|r;
+#endif
+                }
+            }
+            
+            for(long i=begin; i<end; i++) {
+                
+                unsigned int *p = buffer+(i-begin)*w;
+                    
+                sr = sg = sb = 0;
+                
+                for(long k=-(radius+1); k<radius; k++) {
+                    long j2 = 0+k;
+                    if(j2<0) j2=0;
+                    else if(j2>=w-1) j2=w-1;
+                    unsigned int pixel = *(p+j2);
+#ifdef BGRA
+                    sb+=(pixel>>24)&0xFF;
+                    sg+=(pixel>>16)&0xFF;
+                    sr+=(pixel>>8)&0xFF;
+#else
+                    sb+=(pixel>>16)&0xFF;
+                    sg+=(pixel>>8)&0xFF;
+                    sr+=(pixel)&0xFF;
+#endif
+                }
+                
+                unsigned int *q = dst+i;
+                
+                for(long j=0; j<w; j++) {
+                    // sub
+                    long j2 = j-(radius+1);
+                    if(j2<0) j2=0;
+                    unsigned int pixel = *(p+j2);
+#ifdef BGRA
+                    sb-=(pixel>>24)&0xFF;
+                    sg-=(pixel>>16)&0xFF;
+                    sr-=(pixel>>8)&0xFF;
+#else
+                    sb-=(pixel>>16)&0xFF;
+                    sg-=(pixel>>8)&0xFF;
+                    sr-=(pixel)&0xFF;
+#endif
+                    // add
+                    j2 = j+radius;
+                    if(j2>=w-1) j2=w-1;
+                    pixel = *(p+j2);
+#ifdef BGRA
+                    sb+=(pixel>>24)&0xFF;
+                    sg+=(pixel>>16)&0xFF;
+                    sr+=(pixel>>8)&0xFF;
+#else
+                    sb+=(pixel>>16)&0xFF;
+                    sg+=(pixel>>8)&0xFF;
+                    sr+=(pixel)&0xFF;
+#endif
+
+                    unsigned char r = sr*weight;
+                    unsigned char g = sg*weight;
+                    unsigned char b = sb*weight;
+                                        
+#ifdef BGRA
+                    *q = b<<24|g<<16|r<<8|0xFF;
+#else
+                    *q = 0xFF000000|b<<16|g<<8|r;
+#endif
+                    q+=h;
+                }
+            }
+        }
+
+        void blurY(unsigned int *dst, unsigned int *src, unsigned int *buffer, long w, long h, long begin, long end) {
+            
+            long radius = this->_radius;
+            if(radius<=1) radius = 1;
+            else if(radius>=(w>>2)) radius = w>>2;
+            else if(radius>=(h>>2)) radius = h>>2;
+                
+            double weight = 1.0/(double)(radius*2+1);
+                
+            unsigned int sr = 0;
+            unsigned int sg = 0;
+            unsigned int sb = 0;
+            
+            unsigned int *buf = buffer;
+
+            for(long j=begin; j<end; j++) {
+                
+                sr = sg = sb = 0;
+                
+                unsigned int *p = src+j*h;
+                
+                for(long k=-(radius+1); k<radius; k++) {
+                    long i2 = 0+k;
+                    if(i2<0) i2 = 0;
+                    else if(i2>=h-1) i2=h-1;
+                    unsigned int pixel = *(p+i2);
+#ifdef BGRA
+                    sb+=(pixel>>24)&0xFF;
+                    sg+=(pixel>>16)&0xFF;
+                    sr+=(pixel>>8)&0xFF;
+#else
+                    sb+=(pixel>>16)&0xFF;
+                    sg+=(pixel>>8)&0xFF;
+                    sr+=(pixel)&0xFF;
+#endif
+                }
+                
+                for(long i=0; i<h; i++) {
+                    // sub
+                    long i2 = i-(radius+1);
+                    if(i2<0) i2 = 0;
+                    unsigned int pixel = *(p+i2);
+#ifdef BGRA
+                    sb-=(pixel>>24)&0xFF;
+                    sg-=(pixel>>16)&0xFF;
+                    sr-=(pixel>>8)&0xFF;
+#else
+                    sb-=(pixel>>16)&0xFF;
+                    sg-=(pixel>>8)&0xFF;
+                    sr-=(pixel)&0xFF;
+#endif
+                    // add
+                    i2 = i+radius;
+                    if(i2>=h) i2 = h-1;
+                    pixel = *(p+i2);
+#ifdef BGRA
+                    sb+=(pixel>>24)&0xFF;
+                    sg+=(pixel>>16)&0xFF;
+                    sr+=(pixel>>8)&0xFF;
+#else
+                    sb+=(pixel>>16)&0xFF;
+                    sg+=(pixel>>8)&0xFF;
+                    sr+=(pixel)&0xFF;
+#endif
+                                
+                    unsigned char r = sr*weight;
+                    unsigned char g = sg*weight;
+                    unsigned char b = sb*weight;
+#ifdef BGRA
+                    *buf++ = b<<24|g<<16|r<<8|0xFF;
+#else
+                    *buf++ = 0xFF000000|b<<16|g<<8|r;
+#endif
+                }
+            }
+            
+            for(long j=begin; j<end; j++) {
+                    
+                sr = sg = sb = 0;
+                
+                unsigned int *p = buffer+(j-begin)*h;
+                
+                for(long k=-(radius+1); k<radius; k++) {
+                    long i2 = 0+k;
+                    if(i2<0) i2 = 0;
+                    else if(i2>=h-1) i2=h-1;
+                    unsigned int pixel = *(p+i2);
+#ifdef BGRA
+                    sb+=(pixel>>24)&0xFF;
+                    sg+=(pixel>>16)&0xFF;
+                    sr+=(pixel>>8)&0xFF;
+#else
+                    sb+=(pixel>>16)&0xFF;
+                    sg+=(pixel>>8)&0xFF;
+                    sr+=(pixel)&0xFF;
+#endif
+                }
+                
+                unsigned int *q = dst+j;
+                
+                for(long i=0; i<h; i++) {
+                    // sub
+                    long i2 = i-(radius+1);
+                    if(i2<0) i2 = 0;
+                    unsigned int pixel = *(p+i2);
+#ifdef BGRA
+                    sb-=(pixel>>24)&0xFF;
+                    sg-=(pixel>>16)&0xFF;
+                    sr-=(pixel>>8)&0xFF;
+#else
+                    sb-=(pixel>>16)&0xFF;
+                    sg-=(pixel>>8)&0xFF;
+                    sr-=(pixel)&0xFF;
+#endif
+                    // add
+                    i2 = i+radius;
+                    if(i2>=h) i2 = h-1;
+                    pixel = *(p+i2);
+#ifdef BGRA
+                    sb+=(pixel>>24)&0xFF;
+                    sg+=(pixel>>16)&0xFF;
+                    sr+=(pixel>>8)&0xFF;
+#else
+                    sb+=(pixel>>16)&0xFF;
+                    sg+=(pixel>>8)&0xFF;
+                    sr+=(pixel)&0xFF;
+#endif
+
+                    unsigned char r = sr*weight;
+                    unsigned char g = sg*weight;
+                    unsigned char b = sb*weight;
+#ifdef BGRA
+                    *q = b<<24|g<<16|r<<8|0xFF;
+#else
+                    *q = 0xFF000000|b<<16|g<<8|r;
+#endif
+                    q+=w;
+                }
+            }
+        }
+        
+    
+    public :
+    
+        Blur() {
+            
+        }
+        
+        ~Blur() {
+            
+            if(this->_xy) delete[] this->_xy;
+            if(this->_yx) delete[] this->_yx;
+            
+            for(int k=0; k<thread; k++) {
+                if(this->_rgb[k]) delete[] this->_rgb[k];
+            }
+                                        
+            for(int k=0; k<thread; k++) {
+                if(this->_buffer[k]) delete[] this->_buffer[k];
+            }
+        }
+    
+        void set(std::string key, void *value) {
+            if(key=="radius") this->_radius = *((long *)value);
+        }
+    
+        void calc(unsigned int *bop, unsigned int *bip, long width, long height, long rowBytes) {
+            
+            if(this->_radius>=1) {
+                
+                if(!this->_init) {
+                    this->_init = true;
+                    
+                    this->_width  = width;
+                    this->_height = height;
+                    
+                    this->reset();
+                }
+                
+                if(!(this->_width==width&&this->_height==height)) {
+                    
+                    this->_width  = width;
+                    this->_height = height;
+                    
+                    this->reset();
+                }
+                
+                for(long i=0; i<height; i++) {
+                    memcpy((void *)(this->_xy+i*width),(void *)(bip+i*rowBytes),width*sizeof(unsigned int));
+                }
+                
+                long col = height/this->thread;
+                
+                for(int k=0; k<this->thread; k++) {
+                                
+                    if(k==this->thread-1) {
+                        dispatch_group_async(_group,_queue,^{
+                            this->blurX(this->_yx,this->_xy,this->_buffer[k],width,height,col*k,height);
+                        });
+                    }
+                    else {
+                        dispatch_group_async(_group,_queue,^{
+                            this->blurX(this->_yx,this->_xy,this->_buffer[k],width,height,col*k,col*(k+1));
+                        });
+                    }
+                }
+                
+                dispatch_group_wait(_group,DISPATCH_TIME_FOREVER);
+                
+                long row = width/this->thread;
+
+                for(int k=0; k<this->thread; k++) {
+                                
+                    if(k==this->thread-1) {
+                        dispatch_group_async(_group,_queue,^{
+                            this->blurY(this->_xy,this->_yx,this->_buffer[k],width,height,row*k,width);
+                        });
+                    }
+                    else {
+                        dispatch_group_async(_group,_queue,^{
+                            this->blurY(this->_xy,this->_yx,this->_buffer[k],width,height,row*k,row*(k+1));
+                        });
+                    }
+                    
+                }
+                dispatch_group_wait(_group,DISPATCH_TIME_FOREVER);
+                
+                for(long i=0; i<height; i++) {
+            
+                    memcpy((void *)(bop+i*rowBytes),(void *)(this->_xy+i*width),width*sizeof(unsigned int));
+                }
+            }
+            else {
+                
+                memcpy((void *)bop,(void *)bip,width*rowBytes*sizeof(unsigned int));
+            }
+        }
+};
---
+++ jit.blur/jit.blur.mm
@@ -0,0 +1,191 @@
+#import <Cocoa/Cocoa.h>
+
+#include "jit.common.h"
+#include "max.jit.mop.h"
+
+#define t_matrix_info t_jit_matrix_info
+
+#include <string>
+
+#ifdef IS_JIT_LIBRARY_AVAILABLE
+    #define BGRA
+#endif
+
+#include "Blur.h"
+
+#define NAME "jit_blur"
+
+typedef struct _jit_blur {
+    t_object ob;
+    Blur *blur;
+    long radius;
+} t_jit_blur;
+
+typedef struct _max_jit_blur {
+    t_object ob;
+    void *obex;
+} t_max_jit_blur;
+
+static t_class *_jit_blur_class = nullptr;
+static t_class *max_jit_blur_class = nullptr;
+
+NSMutableString *jit_blur_mxo_name() {
+    NSMutableArray *arr = [[[NSString stringWithFormat:@"%s",NAME] componentsSeparatedByString:@"_"] mutableCopy];
+    NSMutableString *str = [NSMutableString stringWithString:arr[0]];
+    for(int n=1; n<arr.count; n++) {
+        [str appendString:@"."];
+        [str appendString:arr[n]];
+    }
+    return str;
+}
+
+t_jit_blur *jit_blur_new(void) {
+    
+    t_jit_blur *x = (t_jit_blur *)jit_object_alloc(_jit_blur_class);
+    if(x) {
+        x->blur = new Blur();
+        x->radius = 1;
+    }
+    return x;
+}
+
+void jit_blur_free(t_jit_blur *x) {
+    if(x&&x->blur) {
+        delete x->blur;
+        x->blur = nullptr;
+    }
+}
+
+t_jit_err jit_blur_matrix_calc(t_jit_blur *x, void *inputs, void *outputs) {
+    
+    t_jit_err err=JIT_ERR_NONE;
+    long in_savelock,out_savelock;
+
+    void *in_matrix = jit_object_method(inputs,_jit_sym_getindex,0);
+    void *out_matrix = jit_object_method(outputs,_jit_sym_getindex,0);
+
+    if(x&&in_matrix&&out_matrix) {
+                
+        in_savelock = (long)jit_object_method(in_matrix,_jit_sym_lock,1);
+        out_savelock = (long)jit_object_method(out_matrix,_jit_sym_lock,1);
+
+        t_jit_matrix_info in_minfo;
+        unsigned char *in_bp;
+        
+        jit_object_method(in_matrix,_jit_sym_getinfo,&in_minfo);
+        jit_object_method(in_matrix,_jit_sym_getdata,&in_bp);
+
+        t_jit_matrix_info out_minfo;
+        unsigned char *out_bp;
+        
+        jit_object_method(out_matrix,_jit_sym_getinfo,&out_minfo);
+        jit_object_method(out_matrix,_jit_sym_getdata,&out_bp);
+
+        if(!in_bp) {
+            err=JIT_ERR_INVALID_INPUT;
+            goto out;
+        }
+        if(!out_bp) {
+            err=JIT_ERR_INVALID_OUTPUT;
+            goto out;
+        }
+
+        if((in_minfo.type!=_jit_sym_char)||(in_minfo.type!=out_minfo.type)) {
+            err=JIT_ERR_MISMATCH_TYPE;
+            goto out;
+        }
+
+        if((in_minfo.planecount!=4)||(out_minfo.planecount!=4)) {
+            err=JIT_ERR_MISMATCH_PLANE;
+            goto out;
+        }
+        
+        if((in_minfo.dimcount!=2)||(out_minfo.dimcount!=2)) {
+            err=JIT_ERR_MISMATCH_DIM;
+            goto out;
+        }
+        
+        x->blur->calc((unsigned int *)out_bp,(unsigned int *)in_bp,in_minfo.dim[0],in_minfo.dim[1],in_minfo.dimstride[1]>>2);
+    }
+    else {
+        return JIT_ERR_INVALID_PTR;
+    }
+
+out:
+    
+    jit_object_method(out_matrix,_jit_sym_lock,out_savelock);
+    jit_object_method(in_matrix,_jit_sym_lock,in_savelock);
+    return err;
+}
+
+void jit_blur_radius(t_jit_blur *x, t_symbol *s, long argc, t_atom *argv) {
+    if(x->blur) {
+        long value = jit_atom_getlong(argv);
+        x->blur->set("radius",(void *)(&value));
+    }
+}
+
+t_jit_err jit_blur_init() {
+    
+    _jit_blur_class = (t_class *)jit_class_new(NAME,(method)jit_blur_new,(method)jit_blur_free, sizeof(t_jit_blur),0);
+
+    t_jit_object *mop = (t_jit_object *)jit_object_new(_jit_sym_jit_mop,1,1);
+    jit_mop_single_type(mop,_jit_sym_char);
+    jit_mop_single_planecount(mop,4);
+    jit_class_addadornment(_jit_blur_class,mop);
+    jit_class_addmethod(_jit_blur_class,(method)jit_blur_matrix_calc,"matrix_calc",A_CANT,0);
+        
+    long attrflags = JIT_ATTR_GET_OPAQUE_USER | JIT_ATTR_SET_USURP_LOW;
+    t_jit_object *attr = (t_jit_object *)jit_object_new(_jit_sym_jit_attr_offset,"radius",_jit_sym_long,attrflags,
+                              (method)0L,(method)jit_blur_radius,calcoffset(t_jit_blur,radius));
+    jit_class_addattr(_jit_blur_class,attr);
+    object_addattr_parse(attr,"label",_jit_sym_symbol,0,"Radius");
+    
+    
+    jit_class_register(_jit_blur_class);
+
+    return JIT_ERR_NONE;
+}
+
+void *max_jit_blur_new(t_symbol *s, long argc, t_atom *argv) {
+    
+    t_max_jit_blur *x = (t_max_jit_blur *)max_jit_object_alloc(max_jit_blur_class,gensym(NAME));
+
+    if(x) {
+        void *o = jit_object_new(gensym(NAME));
+        if(o) {
+            max_jit_mop_setup_simple(x,o,argc,argv);
+            max_jit_attr_args(x,argc,argv);
+        }
+        else {
+            
+            NSMutableString *str = jit_blur_mxo_name();
+            [str appendString:@": could not allocate object"];
+            
+            jit_object_error((t_object *)x,(char *)[str UTF8String]);
+            freeobject((t_object *) x);
+            x = NULL;
+        }
+    }
+    return x;
+}
+
+void max_jit_blur_free(t_max_jit_blur *x) {
+    
+    max_jit_mop_free(x);
+    jit_object_free(max_jit_obex_jitob_get(x));
+    max_jit_object_free(x);
+}
+
+C74_EXPORT void ext_main(void *r) {
+    
+    jit_blur_init();
+    t_class *max_class = class_new([jit_blur_mxo_name() UTF8String],(method)max_jit_blur_new,(method)max_jit_blur_free,sizeof(t_max_jit_blur),NULL,A_GIMME,0);
+    max_jit_class_obex_setup(max_class,calcoffset(t_max_jit_blur,obex));
+    t_class *jit_class = (t_class *)jit_class_findbyname(gensym(NAME));
+    max_jit_class_mop_wrap(max_class,jit_class,0);
+    max_jit_class_wrap_standard(max_class,jit_class,0);
+    class_addmethod(max_class,(method)max_jit_mop_assist,"assist",A_CANT,0);
+    class_register(CLASS_BOX,max_class);
+    max_jit_blur_class = max_class;
+}
